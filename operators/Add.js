44cebb826535a34dc7ed8233f0659c22a43a523d
gidebus <40724004+GiDeBus@users.noreply.github.com>
2025-06-18 18:55:41 -0400

    ADD: Addition operator subclass

---
+++ operators/Add.js
@@ -0,0 +1,13 @@
+const Operator = require("./Operator");
+
+class Add extends Operator {
+  constructor() {
+    super('+');
+  }
+
+  apply(operands) {
+    return operands.reduce((a, b) => a + b , 0);
+  }
+}
+
+module.exports = Add;
