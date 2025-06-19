f5254ce4cfabdebf2abd775a1a495a26a7f1dfaf
gidebus <40724004+GiDeBus@users.noreply.github.com>
2025-06-19 12:16:50 -0400

    EDIT: improve test coverage

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -6,34 +6,113 @@ describe('Calculator', () => {
   beforeEach(() => {
     calc = new Calculator(operators);
   })
-
+  
   test('Pushing elements to stack', () => {
     calc.push(5);
     calc.push('+');
     expect(calc.getStack().length).toBe(2);
   });
 
-  test('Addition of two elements', () => {
-    calc.push(7);
-    calc.push(13);
-    expect(calc.calculate('+')).toBe(20);
-  })
-
-  test('Subtraction of two elements', () => {
-    calc.push(6);
+  test('Invalid operator is used', () => {
     calc.push(3);
-    expect(calc.calculate('-')).toBe(3);
+    expect(() => calc.calculate('$')).toThrow('Unknown operator: $');
   })
 
-  test('Multiplication of two elements', () => {
-    calc.push(5);
-    calc.push(5);
-    expect(calc.calculate('*')).toBe(25);
+  test('Not enough operands given', () => {
+    calc.push(3);
+    expect(() => calc.calculate('+')).toThrow('Not enough operands');
   })
 
-  test('Division of two elements', () => {
-    calc.push(25);
-    calc.push(5);
-    expect(calc.calculate('/')).toBe(5);
+  describe('Addition', () => {
+    test('Addition of two elements', () => {
+      calc.push(7);
+      calc.push(13);
+      expect(calc.calculate('+')).toBe(20);
+    })
+
+    test('Add negative and positive', () => {
+      calc.push(5);
+      calc.push(-4);
+      expect(calc.calculate('+')).toBe(1);
+    })
+
+    test('Add two negatives', () => {
+      calc.push(-5);
+      calc.push(-4);
+      expect(calc.calculate('+')).toBe(-9);
+    })    
+
+    test('Add Zeros', () => {
+      calc.push(0);
+      calc.push(0);
+      expect(calc.calculate('+')).toBe(0);
+    })
+  })
+
+  describe('Subtraction', () => {
+    test('Subtraction of two elements', () => {
+      calc.push(6);
+      calc.push(3);
+      expect(calc.calculate('-')).toBe(3);
+    })
+
+    test('Subtraction positive and negative', () => {
+      calc.push(6);
+      calc.push(-3);
+      expect(calc.calculate('-')).toBe(9);
+    })
+
+    test('Subtraction of two negatives', () => {
+      calc.push(-6);
+      calc.push(-3);
+      expect(calc.calculate('-')).toBe(-3);
+    })  
+
+    test('Subtraction from zero', () => {
+      calc.push(0);
+      calc.push(3);
+      expect(calc.calculate('-')).toBe(-3);
+    })  
+  })
+
+  describe('Multiplication', () => {
+    test('Multiplication of two elements', () => {
+      calc.push(5);
+      calc.push(5);
+      expect(calc.calculate('*')).toBe(25);
+    })
+
+    test('Multiplication by 0', () => {
+      calc.push(5);
+      calc.push(0);
+      expect(calc.calculate('*')).toBe(0);
+    })
+
+    test('Multiplication of negatives', () => {
+      calc.push(-5);
+      calc.push(5);
+      expect(calc.calculate('*')).toBe(-25);
+    })
+  })
+  
+
+  describe('Division', () => {
+    test('Division of two elements', () => {
+      calc.push(25);
+      calc.push(5);
+      expect(calc.calculate('/')).toBe(5);
+    })
+
+    test('Division by zero', () => {
+      calc.push(25);
+      calc.push(0);
+      expect(() => calc.calculate('/')).toThrow('Division by zero is not allowed');
+    })
+
+    test('Division zero by an element', () => {
+      calc.push(0);
+      calc.push(5);
+      expect(calc.calculate('/')).toBe(0);
+    })
   })
 });
